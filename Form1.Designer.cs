48f5a55ca6426c24d1c4fbe8d6679340e66a8095
Luka <36271279+NotJ3st3r@users.noreply.github.com>
2020-02-15 12:47:51 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Kylggr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_RunningProcess = new System.Windows.Forms.Label();
+            this.tmr_ActiveWindow = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl_RunningProcess
+            // 
+            this.lbl_RunningProcess.AutoSize = true;
+            this.lbl_RunningProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lbl_RunningProcess.Location = new System.Drawing.Point(12, 9);
+            this.lbl_RunningProcess.Name = "lbl_RunningProcess";
+            this.lbl_RunningProcess.Size = new System.Drawing.Size(109, 39);
+            this.lbl_RunningProcess.TabIndex = 0;
+            this.lbl_RunningProcess.Text = "label1";
+            // 
+            // tmr_ActiveWindow
+            // 
+            this.tmr_ActiveWindow.Enabled = true;
+            this.tmr_ActiveWindow.Interval = 10;
+            this.tmr_ActiveWindow.Tick += new System.EventHandler(this.tmr_ActiveWindow_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 61);
+            this.Controls.Add(this.lbl_RunningProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_RunningProcess;
+        private System.Windows.Forms.Timer tmr_ActiveWindow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kylggr
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(int i);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private List<string> _keysList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            var buff = new StringBuilder(nChars);
+            var handle = GetForegroundWindow();
+
+            return GetWindowText(handle, buff, nChars) > 0 ? buff.ToString() : null;
+        }
+
+        private void Listener()
+        {
+            string _testerString = "";
+            for (var i = 0; i < 255; i++)
+            {
+                var keyState = GetAsyncKeyState(i);
+                if (keyState != 1 && keyState != -32767) continue;
+
+                _keysList.Add(Convert.ToString((char) i));
+
+                //if ((i >= 65 && i <= 90) || (i >= 97 && i <= 122))
+                //{
+                //    var character = (char)i;
+                //    _testerString += character.ToString().ToLower(CultureInfo.CurrentCulture);
+                //}
+                //else if (i == 8)
+                //{
+                //    if (string.IsNullOrEmpty(_testerString)) return;
+                //    _testerString = _testerString?.Substring(0, _testerString.Length - 1);
+                //}
+            }
+        }
+
+        private void tmr_ActiveWindow_Tick(object sender, EventArgs e)
+        {
+            lbl_RunningProcess.Text = GetActiveWindowTitle();
+        }
+    }
+}
